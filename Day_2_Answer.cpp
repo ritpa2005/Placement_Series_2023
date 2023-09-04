dbd2ad8c64a84a973dc563788d7b1d047480b32c
Ritesh Kumar Panda <ritesh.ku.panda.925@gmail.com>
2023-09-04 17:53:11 +0530

    Day 2 work

---
+++ Day_2_Answer.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int h,r;
+    cout<<"Enter height and radius: ";
+    cin>>h>>r;
+    cout<<"Volume = "<< (3.14*r*r*h)<<endl;
+    return 0;
+}
