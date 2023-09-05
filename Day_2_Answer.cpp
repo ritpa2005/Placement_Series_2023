7e88007d13688f4e6d451283b2661d4a7abfe5bb
Ritesh Kumar Panda <ritesh.ku.panda.925@gmail.com>
2023-09-05 21:21:03 +0530

    Update Day_2_Answer.cpp

--- Day_2_Answer.cpp
+++ Day_2_Answer.cpp
@@ -5,6 +5,6 @@ int main(){
     int h,r;
     cout<<"Enter height and radius: ";
     cin>>h>>r;
-    cout<<"Volume = "<< (3.14*r*r*h)<<endl;
+    cout<<"Volume = "<< (3.14*r*r*h/3)<<endl;
     return 0;
 }
