dda5f9b52afc274bae392c6e8b8a5f2fc1998d7c
Ritesh Kumar Panda <ritesh.ku.panda.925@gmail.com>
2023-09-17 01:57:46 +0530

    Day 6 completed

---
+++ Day_6_Answer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<2*i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    for(int i=n-2; i>0; i--){
+        for(int j=1; j<2*i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
